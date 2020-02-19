a4d899593139ec86195528111f56d1f760ac57af
brian-bourdon <brian78.b@gmail.com>
2020-02-19 13:04:47 +0100

    Add files via upload

--- src/CreneauTest.java
+++ src/CreneauTest.java
@@ -1,11 +1,17 @@
 import java.util.Date;
 import java.time.LocalTime;
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CreneauTest {
     LocalTime localTime = LocalTime.now();
     Creneau creneau1 = new Creneau(new Date(), localTime, localTime.plusHours(2));
     Creneau creneau2 = new Creneau(new Date(), localTime, localTime.plusHours(2));
+    String raison = "trop compétent";
+
+    Creneau creneau3 = new Creneau(new Date(), localTime, localTime.plusHours(2));
+    Entretien entretien = new Entretien( creneau3, "confirmer", "Brian", "Kyriel");
 
     @org.junit.jupiter.api.Test
     void getDate() {
@@ -24,6 +30,16 @@ class CreneauTest {
     @org.junit.jupiter.api.Test
     void equals() {
         assertTrue(creneau1.equals(creneau2));
+    }
+
+    @org.junit.jupiter.api.Test
+    void confirmer() {
+          assertTrue(entretien.confirmer());
+    }
+
+    @org.junit.jupiter.api.Test
+    void annuler() {
+        assertTrue(entretien.annuler(raison));
     }
 
 }
